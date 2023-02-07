25d2bec67f238b25bb8817051ebc9cf748829bb7
nicolima200 <nicolima200@gmail.com>
2023-02-07 11:10:16 -0300

    Cambie CrossOrigins en las clases controller a https://nicoportfolioweb.web.app

--- src/main/java/com/miportfolio/nico/Controller/CHyS.java
+++ src/main/java/com/miportfolio/nico/Controller/CHyS.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://backendnico.onrender.com/")
+@CrossOrigin(origins = "https://nicoportfolioweb.web.app/")
 @RequestMapping("/skill")
 public class CHyS {
 
--- src/main/java/com/miportfolio/nico/Security/Controller/AuthController.java
+++ src/main/java/com/miportfolio/nico/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 //@CrossOrigin(origins = "https://localhost:4200")
-@CrossOrigin(origins = "https://nicoportfolioweb.web.app")
+@CrossOrigin(origins = "https://nicoportfolioweb.web.app/")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
