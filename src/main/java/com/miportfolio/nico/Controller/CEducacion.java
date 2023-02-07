9ac9e5de3e19cdcf73cbf5242f1f0fd36e7c1c0a
nicolima200 <nicolima200@gmail.com>
2023-02-07 11:34:13 -0300

    Anulado el crossorigins en los controllers excepto en authcontroller

--- src/main/java/com/miportfolio/nico/Controller/CEducacion.java
+++ src/main/java/com/miportfolio/nico/Controller/CEducacion.java
@@ -10,7 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/educacion")
 //@CrossOrigin(origins="http://localhost:4200")
-@CrossOrigin(origins = "https://nicoportfolioweb.web.app/")
+//@CrossOrigin(origins = "https://nicoportfolioweb.web.app/")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/miportfolio/nico/Controller/PersonaController.java
+++ src/main/java/com/miportfolio/nico/Controller/PersonaController.java
@@ -5,7 +5,7 @@ import com.miportfolio.nico.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://nicoportfolioweb.web.app/")
+//@CrossOrigin(origins = "https://nicoportfolioweb.web.app/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
